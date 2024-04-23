446bb4c03506c3ff0f44f5450179691086521ec1
briankiralyfalvi <111707786+briankiralyfalvi@users.noreply.github.com>
2024-04-22 21:55:09 -0500

    Fixed File in MPI

--- 3way-mpi/main.c
+++ 3way-mpi/main.c
@@ -11,11 +11,11 @@
 #if RUNNING_LOCAL
     #define TEXT_FILE "../../wiki_dump.txt"
 #else
-    //#define TEXT_FILE "~dan/625/wiki_dump.txt"
-    #define TEXT_FILE "test.txt"
+    #define TEXT_FILE "/homes/dan/625/wiki_dump.txt"
+    //#define TEXT_FILE "test.txt"
 #endif
-//#define NUMBER_OF_LINES 1000000
-#define NUMBER_OF_LINES 4
+#define NUMBER_OF_LINES 1000000
+//#define NUMBER_OF_LINES 1000
 
 #pragma endregion
 
@@ -78,7 +78,7 @@ int main()
     if (taskID == 0){ //For the main process
         for(int i = 1; i < numProcesses; ++i){
             uint32_t start_line = linesPerProcess * i; //Calculates the start line for each process
-            MPI_Send(&start_line, 1, MPI_INT, i, 0, MPI_COMM_WORLD); //Send the start line to each process.
+            MPI_Send(&start_line, 1, MPI_UINT32_T, i, 0, MPI_COMM_WORLD); //Send the start line to each process.
         }
 
         uint32_t start_line = 0; //start line for the main process
@@ -90,25 +90,20 @@ int main()
         }
 
         for(int i = 1; i < numProcesses; ++i){ //Loop to print each process's work
-            MPI_Recv(biggest_char_single_process, linesPerProcess, MPI_INT, i, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE); //Receive each array
+            MPI_Recv(biggest_char_single_process, linesPerProcess, MPI_UINT8_T, i, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE); //Receive each array
             for(int j = 0; j < linesPerProcess; ++j){  
                 uint32_t index = j + i*linesPerProcess;  // Get the overall index
                 printf("%d: %d\n", index, biggest_char_single_process[j]);      // Print its index and its ascii value
             }
         }
+        free(biggest_char_single_process);
     }
     else{ //For all other processes
         uint32_t start_line;
-        MPI_Recv(&start_line, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE); //Receives the start line
+        MPI_Recv(&start_line, 1, MPI_UINT32_T, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE); //Receives the start line
         processLines(start_line, linesPerProcess, biggest_char_single_process); //process the lines starting at the start line
-        MPI_Send(biggest_char_single_process, linesPerProcess, MPI_INT, 0, 0, MPI_COMM_WORLD); //Sends the array back to the main process
+        MPI_Send(biggest_char_single_process, linesPerProcess, MPI_UINT8_T, 0, 0, MPI_COMM_WORLD); //Sends the array back to the main process
     } 
 
     MPI_Finalize(); //End of MPI
-    free(biggest_char_single_process);
-
-    
-
 }
-
-
